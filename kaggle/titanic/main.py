6bf8c4e2c0928fd341798415bb572c9d4b1ceb6b
Andre Santos <andre.santos0906@gmail.com>
2020-10-27 19:00:02 +0100

    feat: first commit

---
+++ kaggle/titanic/main.py
@@ -0,0 +1,11 @@
+def main():
+    # import a function to train the model
+    # import the data
+    # call a function to preapre the data
+    # train the modal
+    # generate predictions file for the titanic challenge
+    print('main function')
+
+if __name__ == "__main__":
+    main()
+
